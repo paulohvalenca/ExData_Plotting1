0178a88b77542d88eccef2d3c69421f1bbe3275a
paulohvalenca <paulohbvalenca@gmail.com>
2020-08-14 00:26:02 -0300

    :)

--- plot1.R
+++ plot1.R
@@ -1,10 +1,15 @@
+# Loading Data
 library(data.table)
-filename <- "D. Science coursera 4 week swirl leasons.txt"
-data <- fread(filename)
-subdata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+filename <- "household_power_consumption.txt"
+data <- fread(filename) 
 
+# Subseting 
+subdata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 GAP <- as.numeric(subdata$Global_active_power)
+
+# Ploting
 png("plot1.png", width = 480, height = 480)
-hist(globalActivePower, col="red", main="Global Active Power", 
-     xlab="Global Active Power (kilowatts)")
+hist(GAP, col="red", main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)")
+
 dev.off()
